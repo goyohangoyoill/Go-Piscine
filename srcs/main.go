c9bdd82acc20f327bd51ca52c21e4dd768774bd7
splkm97 <splkm123@naver.com>
2021-11-05 00:26:27 +0900

    refactor: remove hardcoded prefix

--- srcs/main.go
+++ srcs/main.go
@@ -18,6 +18,10 @@ import (
 	"github.com/spf13/viper"
 )
 
+const (
+	prefix = "$"
+)
+
 var (
 	c        *client.Client
 	rMIDs    map[string]string
@@ -93,47 +97,47 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	if m.Content == "!명령어" {
+	if m.Content == prefix + "명령어" {
 		sendCommandDetail(s, m)
 		return
 	}
-	if m.Content == "!제출" {
+	if m.Content == prefix + "제출" {
 		submitEmbed := embed.NewEmbed()
 		submitEmbed.SetTitle("제출 명령어는 다음과 같이 입력해야 합니다.")
 		submitEmbed.AddField(
 			"<명령어 예시>",
-			"!제출 <github repo url> <subject name>\n"+
-				"!제출 https://github.com/example123/ExampleRepo Day01")
+			prefix + "제출 <github repo url> <subject name>\n"+
+				prefix + "제출 https://github.com/example123/ExampleRepo Day01")
 		s.ChannelMessageSendEmbed(m.ChannelID, submitEmbed.MessageEmbed)
 		return
 	}
-	if strings.HasPrefix(m.Content, "!제출 ") { // !제출 <github repo url> <subject name>
+	if strings.HasPrefix(m.Content, prefix + "제출 ") { // !제출 <github repo url> <subject name>
 		submissionTask(s, m)
 		return
 	}
-	if m.Content == "!제출취소" {
+	if m.Content == prefix + "제출취소" {
 		submissionCancelTask(s, m)
 		return
 	}
-	if m.Content == "!평가등록" {
+	if m.Content == prefix + "평가등록" {
 		registerEvalTask(s, m)
 		return
 	}
-	if m.Content == "!평가취소" {
+	if m.Content == prefix + "평가취소" {
 		RegisterCancelTask(s, m)
 		return
 	}
-	if m.Content == "!매칭상태" {
+	if m.Content == prefix + "매칭상태" {
 		state := c.MatchState()
 		sendEmbedPretty(s, m.ChannelID, state)
 		return
 	}
-	if m.Content == "!내점수" {
+	if m.Content == prefix + "내점수" {
 		grade := c.MyGrade(m.Author.ID)
 		sendEmbedPretty(s, m.ChannelID, grade)
 		return
 	}
-	if m.Content == "!GOPISCINEREGISTERMODE" && (m.Author.ID == "318743234601811969" ||
+	if m.Content == prefix + "GOPISCINEREGISTERMODE" && (m.Author.ID == "318743234601811969" ||
 		m.Author.ID == "905699384581312542" || m.Author.ID == "382356905990815744" ||
 		m.Author.ID == "383847223504666626") {
 		mode = !mode
@@ -143,25 +147,25 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			s.ChannelMessageSend(m.ChannelID, "사용자 등록모드 종료")
 		}
 	}
-	if mode && strings.HasPrefix(m.Content, "!인트라등록") {
+	if mode && strings.HasPrefix(m.Content, prefix + "인트라등록") {
 		command := strings.Split(m.Content, " ")
 		if len(command) != 2 {
-			s.ChannelMessageSend(m.ChannelID, "사용방법: !인트라등록 <intraID>")
+			s.ChannelMessageSend(m.ChannelID, "사용방법: " + prefix + "인트라등록 <intraID>")
 			return
 		}
 		dmChan, _ := s.UserChannelCreate(m.Author.ID)
 		regMsg, _ := s.ChannelMessageSend(dmChan.ID, "**주의** 등록된 정보는 등록 기간이 지나면 바꿀 수 없음!\n" +
-			"등록된 인트라 ID 를 바꾸고 싶다면 !인트라수정 명령어를 사용하세요\n" +
+			"등록된 인트라 ID 를 바꾸고 싶다면 " + prefix + "인트라수정 명령어를 사용하세요\n" +
 			"당신의 인트라 ID 가 "+command[1]+" 이(가) 맞습니까?")
 		rMIDs[m.Author.ID] = regMsg.ID
 		IntraIDs[m.Author.ID] = command[1]
 		s.MessageReactionAdd(dmChan.ID, regMsg.ID, "⭕")
 		s.MessageReactionAdd(dmChan.ID, regMsg.ID, "❌")
 	}
-	if mode && strings.HasPrefix(m.Content, "!인트라수정") {
+	if mode && strings.HasPrefix(m.Content, prefix + "인트라수정") {
 		command := strings.Split(m.Content, " ")
 		if len(command) != 2 {
-			s.ChannelMessageSend(m.ChannelID, "사용방법: !인트라수정 <intraID>")
+			s.ChannelMessageSend(m.ChannelID, "사용방법: " + prefix + "인트라수정 <intraID>")
 			return
 		}
 		dmChan, _ := s.UserChannelCreate(m.Author.ID)
@@ -247,8 +251,8 @@ func submissionTask(s *discordgo.Session, m *discordgo.MessageCreate) {
 		submitEmbed.SetTitle("제출 명령어는 다음과 같이 입력해야 합니다.")
 		submitEmbed.AddField(
 			"<명령어 예시>",
-			"!제출 <github repo url> <subject name>\n"+
-				"!제출 https://github.com/example123/ExampleRepo Day01")
+			prefix + "제출 <github repo url> <subject name>\n"+
+				prefix + "제출 https://github.com/example123/ExampleRepo Day01")
 		s.ChannelMessageSendEmbed(m.ChannelID, submitEmbed.MessageEmbed)
 		return
 	}
@@ -280,23 +284,23 @@ func sendCommandDetail(s *discordgo.Session, m *discordgo.MessageCreate) {
 	commandDetailEmbed.SetTitle("명령어 목록")
 	commandDetailEmbed.AddField(
 		"도움말 명령어",
-		"!도움말"+"\n"+
-			"!명령어",
+		prefix + "도움말"+"\n"+
+			prefix + "명령어",
 	)
 	commandDetailEmbed.AddField(
 		"제출 명령어",
-		"!제출 <GitRepoURL> <SubjectID>"+"\n"+
-			"!제출취소",
+		prefix + "제출 <GitRepoURL> <SubjectID>"+"\n"+
+			prefix + "제출취소",
 	)
 	commandDetailEmbed.AddField(
 		"평가자 등록 명령어",
-		"!평가등록"+"\n"+
-			"!평가취소",
+		prefix + "평가등록"+"\n"+
+			prefix + "평가취소",
 	)
 	commandDetailEmbed.AddField(
 		"정보 확인 명령어",
-		"!내점수"+"\n"+
-			"!매칭상태",
+		prefix + "내점수"+"\n"+
+			prefix + "매칭상태",
 	)
 	s.ChannelMessageSendEmbed(m.ChannelID, commandDetailEmbed.MessageEmbed)
 }
